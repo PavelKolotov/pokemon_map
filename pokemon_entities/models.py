86b637f2afe6fe260596f20524ca6aaf027d3b0d
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-03-25 16:44:05 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,17 +1,13 @@
 from django.db import models  # noqa F401
 
 
-class MyModel(models.Model):
-    objects = models.Manager()
-
-
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Имя')
     title_en = models.CharField(max_length=200, blank=True, verbose_name='Имя на английском')
     title_jp = models.CharField(max_length=200, blank=True, verbose_name='Имя на японском')
     description = models.TextField(blank=True, verbose_name='Описание')
     image = models.ImageField(upload_to='pokemons', blank=True, verbose_name='Картинка')
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='child',
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='childs',
                                verbose_name='Из кого эволюционировал')
 
 
@@ -20,7 +16,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities', verbose_name='Покемон')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT, related_name='entities', verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(null=True, verbose_name='Дата и время появления')
@@ -31,3 +27,5 @@ class PokemonEntity(models.Model):
     defence = models.IntegerField(null=True, blank=True, verbose_name='Защита')
     stamina = models.IntegerField(null=True, blank=True, verbose_name='Выносливость')
 
+    def __str__(self):
+        return f'{self.pokemon} уровень: {self.level}'
